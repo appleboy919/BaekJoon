e04c75bd752711640bf8f63efb37df67b6719797
appleboy919 <doh24@wisc.edu>
2021-01-27 00:32:20 +0900

    add multiplication

--- IO_and_Operation/add_and_subtract.py
+++ IO_and_Operation/add_and_subtract.py
@@ -1,5 +1,17 @@
+# add_and_subtract
 a, b = input().split()
 sum = int(a)+int(b)
 subtract = int(a)-int(b)
 print("addition:", sum)
 print("subtraction:", subtract)
+
+# multiplication
+x = int(input())
+y = int(input())
+z1 = y % 10
+z2 = (y//10) % 10
+z3 = y//100
+a = x*z1
+b = x*z2
+c = x*z3
+print(a+10*b+100*c)
