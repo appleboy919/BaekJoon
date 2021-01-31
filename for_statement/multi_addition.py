79321ec188cc4c6b6691999e7f7a9bd61f5147c7
appleboy919 <doh24@wisc.edu>
2021-01-31 22:43:47 +0900

    add each pair of integers as tuples

--- for_statement/multi_addition.py
+++ for_statement/multi_addition.py
@@ -5,9 +5,9 @@ for i in range(0, T):
     a, b = input().split()
     a = int(a)
     b = int(b)
-    sum.append(a+b)
+    sum.append((a, b))
 
 i = 1
-for k in sum:
-    print('Case #{}: {}'.format(i, k))
+for k, v in sum:
+    print('Case #{}: {} + {} = {}'.format(i, k, v, k+v))
     i += 1
