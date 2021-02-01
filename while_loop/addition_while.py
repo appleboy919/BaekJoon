d2dd555dcb117d2ea91d0470d6dabf7e0b7f5426
appleboy919 <doh24@wisc.edu>
2021-02-01 22:04:45 +0900

    edit while_addition.py

--- while_loop/addition_while.py
+++ while_loop/addition_while.py
@@ -1,11 +1,11 @@
 sum_list = []
 
-k,v = input().split()
+k, v = input().split()
 k = int(k)
 v = int(v)
-while (k,v) != (0,0):
+while (k, v) != (0, 0):
     sum_list.append(k+v)
-    k,v = input().split()
+    k, v = input().split()
     k = int(k)
     v = int(v)
 
