1201a7467a4c4e688383077d866c31eda5e6e17b
appleboy919 <doh24@wisc.edu>
2021-02-02 23:30:24 +0900

    edit: when N=1

--- Array_1D/average.py
+++ Array_1D/average.py
@@ -1,20 +1,22 @@
 N = int(input())
 scores = list(map(int, input().split()))
 
-max = scores[0]
-max_index = 0
-index = 0
-for i in scores:
-    if i > max:
-        max = i
-        max_index = index
-    index += 1
+if N > 1:
+    max = scores[0]
+    max_index = 0
+    index = 0
+    for i in scores:
+        if i > max:
+            max = i
+            max_index = index
+        index += 1
 
-sum = 0
-index = 0
-for i in scores:
-    if index == max_index:
-        continue
-    sum += i/max * 100
-
-print('%f' % (sum/N))
+    sum = 0
+    index = 0
+    for i in scores:
+        if index == max_index:
+            continue
+        sum += i/max * 100
+else:
+    sum = scores[0]
+print('{}'.format(sum/N))
