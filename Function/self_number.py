29e15859191496488b80b6aef19a357d5434df44
appleboy919 <doh24@wisc.edu>
2021-02-04 00:34:06 +0900

    use set structure to remove non-self_numbers

--- Function/self_number.py
+++ Function/self_number.py
@@ -3,3 +3,15 @@ def d(n):
     for i in str(n):
         number += int(i)
     return number
+
+
+self_nums = set(range(1, 10001))
+
+for i in range(1, 10000):
+    try:
+        self_nums.remove(d(i))
+    except KeyError:
+        pass
+
+for i in self_nums:
+    print(i)
