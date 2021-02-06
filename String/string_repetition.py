7d430725fcf0203218ba916892c5684cefd40afe
appleboy919 <doh24@wisc.edu>
2021-02-06 13:31:06 +0900

    edit: input repeit numbers

--- String/string_repetition.py
+++ String/string_repetition.py
@@ -3,9 +3,10 @@ strings = []
 
 for i in range(N):
     n, s = input().split()
+    n = int(n)
     temp_string = ''
     for c in s:
-        temp_string += c*3
+        temp_string += c*n
     strings.append(temp_string)
 
 for new_string in strings:
