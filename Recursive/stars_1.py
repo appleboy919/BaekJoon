79889bfd47f54a66fc7164f3d8bfd5da6db648d4
appleboy919 <doh24@wisc.edu>
2021-02-27 16:43:20 +0900

    draw stars with the given pattern using recursive function

--- Recursive/stars_1.py
+++ Recursive/stars_1.py
@@ -1,11 +1,33 @@
-def stars(n):
-    if n == 1:
-        s = '***'
+# code reference: https://study-all-night.tistory.com/5
+import math
+
+
+def stars(n, grid):
+    if n == 3:
+        grid[0][:3] = grid[2][:3] = [1]*3
+        grid[1][:3] = [1, 0, 1]
+        return
+    a = n//3
+    stars(a, grid)
+    for i in range(3):
+        for j in range(3):
+            if i == 1 and j == 1:
+                continue
+            for k in range(a):
+                grid[a*i+k][a*j:a*(j+1)] = grid[k][:a]
 
 
 def main():
     N = int(input())
-    print(N**(1/3))
+    grid = [[0 for i in range(N)] for i in range(N)]
+    stars(N, grid)
+    for i in range(N):
+        for j in range(N):
+            if grid[i][j]:
+                print('*', end='')
+            else:
+                print(' ', end='')
+        print()
 
 
 if __name__ == '__main__':
