8820a7c24e83ab5f5fa32b2bdf530af54e9b423a
appleboy919 <doh24@wisc.edu>
2021-03-02 22:08:43 +0900

    divide in cases

--- Recursive/hanoi_tower.py
+++ Recursive/hanoi_tower.py
@@ -1,13 +1,25 @@
-def hanoi(n, l):
+def hanoi(n, l, N):
     if n == 1:
-        l.append(('1 3'))
-        return 1
+        if N % 2 == 0:
+            l.append('1 2')
+        else:
+            l.append('1 3')
+        return
+    hanoi(n - 1, l, N)
+    if l[len(l) - 1][2] == '3':
+        l.append('1 2')
+
+    else:
+        l.append('1 3')
+
+
+## n%2 == 0: 1->2
 
 
 if __name__ == '__main__':
     N = int(input())
     trace = []
-    T = hanoi(N, trace)
-    print(T)
+    hanoi(N, trace, N)
+    print(len(trace))
     for s in trace:
         print(s)
