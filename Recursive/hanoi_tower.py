bb675f508a6d724aeec9e71a1e608daff63debf0
appleboy919 <doh24@wisc.edu>
2021-03-05 21:11:17 +0900

    hanoi algorithm 1

--- Recursive/hanoi_tower.py
+++ Recursive/hanoi_tower.py
@@ -1,25 +1,38 @@
-def hanoi(n, l, N):
+# def hanoi(n, l, N):
+#     if n == 1:
+#         if N % 2 == 0:
+#             l.append('1 2')
+#         else:
+#             l.append('1 3')
+#         return
+#     # recursive call for n-1
+#     hanoi(n - 1, l, N)
+#     peg = int(l[-1][2])
+#
+#
+# ## n%2 == 0: 1->2
+#
+#
+# if __name__ == '__main__':
+#     N = int(input())
+#     trace = []
+#     hanoi(N, trace, N)
+#     print(len(trace))
+#     for s in trace:
+#         print(s)
+
+# codes reference: https://pacific-ocean.tistory.com/119
+
+n = int(input())
+def hanoi(n, a, b, c):
     if n == 1:
-        if N % 2 == 0:
-            l.append('1 2')
-        else:
-            l.append('1 3')
-        return
-    hanoi(n - 1, l, N)
-    if l[len(l) - 1][2] == '3':
-        l.append('1 2')
-
+        print(a, c)
     else:
-        l.append('1 3')
-
-
-## n%2 == 0: 1->2
-
-
-if __name__ == '__main__':
-    N = int(input())
-    trace = []
-    hanoi(N, trace, N)
-    print(len(trace))
-    for s in trace:
-        print(s)
+        hanoi(n - 1, a, c, b)
+        print(a, c)
+        hanoi(n - 1, b, a, c)
+sum = 1
+for i in range(n - 1):
+    sum = sum * 2 + 1
+print(sum)
+hanoi(n, 1, 2, 3)
