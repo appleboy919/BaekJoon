630ceb211b95f34a4a84e01b5afd12190ed53a1a
appleboy919 <doh24@wisc.edu>
2021-03-06 17:10:26 +0900

    hanoi algorithm edit(empty -> temp)

--- Recursive/hanoi_tower.py
+++ Recursive/hanoi_tower.py
@@ -1,10 +1,11 @@
 def hanoi(n, ans, stack, dest, temp):
-    if n==1:
+    if n == 1:
         ans.append(f'{stack} {dest}')
         return
-    hanoi(n-1, ans, stack, temp, dest)
+    hanoi(n - 1, ans, stack, temp, dest)
     ans.append(f'{stack} {dest}')
-    hanoi(n-1, ans, temp, dest, stack)
+    hanoi(n - 1, ans, temp, dest, stack)
+
 
 if __name__ == '__main__':
     N = int(input())
@@ -13,5 +14,3 @@ if __name__ == '__main__':
     print(len(trace))
     for s in trace:
         print(s)
-
-
