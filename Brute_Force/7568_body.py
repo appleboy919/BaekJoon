3e2d0f7fb10cad720ff4483b411441c741273bed
appleboy919 <doh24@wisc.edu>
2021-03-13 02:03:14 +0900

    new: body grade with height and weight

---
+++ Brute_Force/7568_body.py
@@ -0,0 +1,9 @@
+def body_grades(l):
+    grades = []
+    for t in l:
+        pass
+if __name__ == '__main__':
+    N = int(input())
+    l = []
+    for i in range(N):
+        l.append(tuple(map(int, input().split())))
