aa10f53b30d0bec5409849cdd1060f4cd6e6f264
appleboy919 <doh24@wisc.edu>
2021-03-17 01:10:12 +0900

    first try - error: cannot handle with 3 125 185 120 160 20 200

--- Brute_Force/7568_body.py
+++ Brute_Force/7568_body.py
@@ -1,30 +1,38 @@
 def body_grades(l, a):
-    len = len(l)
+    length = len(l)
     temp = []
-    index = 0
     grade = 1
-    maxH = -1
-    maxW = -1
     max_index = 0
-    same_grades = False
-    while grade > len:
-        # find the largest
-        if not a[index] and maxH <= l[index][0]:
-            max_index = index
-            maxH = l[index][0]
-            if maxW == -1:
-                maxW = l[index][1]
-            else:
-                if maxW > l[index][1]:
-                    temp.append(index)
-                    same_grades = True
+    max_data = None
 
-        index += 1
-    index = 0
-    maxW = l[max_index][1]
-    while index < len:
-        if not a[index]:
-            if
+    while grade <= length:
+        # initialize max_data and temp list before getting into inner loop
+        max_data = None
+        temp = []
+        # start inner loop for biggest body data from the rest
+        for i in range(length):
+            if not a[i]:
+                if max_data:
+                    if max_data[0] < l[i][0] and max_data[1] < l[i][1]:
+                        max_data = l[i]
+                        max_index = i
+                        temp.clear()
+                    elif max_data[0] > l[i][0] and max_data[1] > l[i][1]:
+                        pass
+                    else:
+                        temp.append(i)
+                else:
+                    max_data = l[i]
+                    max_index = i
+
+        if temp:  # if there are ties in the data
+            temp.append(max_index)
+            for j in temp:
+                a[j] = grade
+            grade += len(temp)
+        else:  # no ties
+            a[max_index] = grade
+            grade += 1
 
 
 if __name__ == '__main__':
@@ -33,5 +41,7 @@ if __name__ == '__main__':
     a = [0] * N
     for i in range(N):
         l.append(tuple(map(int, input().split())))
-    print(l)
-    print(a)
+    body_grades(l, a)
+    for i in a:
+        print(i, end=' ')
+    print()
