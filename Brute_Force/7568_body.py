a5387bf4da285d7a5a887ec6ca3f7bbac6f639af
appleboy919 <doh24@wisc.edu>
2021-03-17 19:35:40 +0900

    complete: "grade = bigger data num + 1"

--- Brute_Force/7568_body.py
+++ Brute_Force/7568_body.py
@@ -1,45 +1,15 @@
 def body_grades(l, a):
-    # group each data with same tier
-    temp = []
-    jump = False
-    for i in range(len(l)):
-        jump = False
-        if not temp:
-            temp.append([i])
-        else:
-            # TODO: the problem is here!! (order of grouping)
-            for same_grade in temp:
-                for data in same_grade:
-                    if l[data][0] > l[i][0] and l[data][1] > l[i][1]:
-                        pass
-                    elif l[data][0] < l[i][0] and l[data][1] < l[i][1]:
-                        pass
-                    else:
-                        same_grade.append(i)
-                        jump = True
-                        break
-                if jump:
-                    break
-            if not jump:
-                temp.append([i])
-
-    # grade each group of data
-    grade = 1
-    grades = []
-    for i in range(len(temp)):
-        grades.append((l[temp[i][0]][0], i))
-    grades = sorted(grades, reverse=True)
-    for g in grades:
-        for k in temp[g[1]]:
-            a[k] = grade
-        grade += len(temp[g[1]])
-    print('Done!')
+    n = len(l)
+    for i in range(n):
+        for j in range(n):
+            if l[i][0] < l[j][0] and l[i][1] < l[j][1]:
+                a[i] += 1
 
 
 if __name__ == '__main__':
     N = int(input())
     l = []
-    a = [0] * N
+    a = [1] * N
     for i in range(N):
         l.append(tuple(map(int, input().split())))
     body_grades(l, a)
