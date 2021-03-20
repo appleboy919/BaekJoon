4da111ea1f366f6838825fcf19c84ecbc403a6e5
appleboy919 <doh24@wisc.edu>
2021-03-20 23:33:31 +0900

    complete: used merge sort

--- Sorting/2750_sort_number.py
+++ Sorting/2750_sort_number.py
@@ -1,5 +1,32 @@
 def merge_sort(list):
-
+    n = len(list)
+    if n == 1:
+        return list
+    elif n == 2:
+        if list[0] > list[1]:
+            return list[::-1]
+        else:
+            return list
+    L = merge_sort(list[:n // 2])
+    R = merge_sort(list[n // 2:])
+    l = len(L)
+    r = len(R)
+    i = j = 0
+    sort_list = []
+    while not (i == l and j == r):
+        if i == l:
+            sort_list.append(R[j])
+            j += 1
+        elif j == r:
+            sort_list.append(L[i])
+            i += 1
+        elif L[i] < R[j]:
+            sort_list.append(L[i])
+            i += 1
+        else:
+            sort_list.append(R[j])
+            j += 1
+    return sort_list
 
 if __name__ == '__main__':
     N = int(input())
