f72a75c451436f43866dcc9145ea63836950194e
appleboy919 <doh24@wisc.edu>
2021-03-22 23:58:04 +0900

    ongoing: still need to decrease memory use

--- Sorting/10989_counting_sort.py
+++ Sorting/10989_counting_sort.py
@@ -1,34 +1,37 @@
 import sys
 
 
-def count_sort(nums):
-    counts = list(range(10001))
+def count_sort(N):
+    counts = [0] * 10001
 
     # count each numbers
-    for i in nums:
-        counts[i] += 1
+    i = 0
+    while i < N:
+        counts[int(sys.stdin.readline())] += 1
+        i += 1
 
     # modify count array to store sum of each previous counts
-    for i in range(10001):
-        counts[i] += counts[i - 1]
+    index = 0
+    while index < 10001:
+        counts[index] += counts[index - 1]
+        index += 1
 
     ans = []
     # print each numbers from the counts
     count = 0
-    for i in range(10001):
+    i = 0
+    while i < 10001:
         counts[i] -= count
         while counts[i] != 0:
             ans.append(i)
             count += 1
             counts[i] -= 1
+        i += 1
     return ans
 
 
 if __name__ == '__main__':
     N = int(sys.stdin.readline())
-    l = []
-    for i in range(N):
-        l.append(int(sys.stdin.readline()))
-    sorted_l = count_sort(l)
-    for i in sorted_l:
+    sorted_list = count_sort(N)
+    for i in sorted_list:
         sys.stdout.write(f'{i}' + '\n')
