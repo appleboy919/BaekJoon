b994022085cfb7a738127cf11ae2350cb02b2184
appleboy919 <doh24@wisc.edu>
2021-03-27 16:48:09 +0900

    cont: try to merge sort the tuples

--- Sorting/11650_sort_coordinates.py
+++ Sorting/11650_sort_coordinates.py
@@ -1,6 +1,10 @@
 import sys
 
 def print_sorted_coordinates(n):
+    coordinates = []
+    for i in range(n):
+        coordinates.append(tuple(map(int, sys.stdin.readline().split())))
+
 
 if __name__ == '__main__':
     N = int(sys.stdin.readline())
