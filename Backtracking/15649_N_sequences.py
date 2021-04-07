a5757f92fa3b21028402effd6ab04d2009d2c0a9
appleboy919 <doh24@wisc.edu>
2021-04-07 18:38:18 +0900

    cont: mostly done, but need to figure out errors for '4 4'

--- Backtracking/15649_N_sequences.py
+++ Backtracking/15649_N_sequences.py
@@ -13,16 +13,24 @@ def print_sequences(m, n):
     current_num = m
     index = m - 1
 
+    # set the first sequence and its number booleans to False
     for i in range(m):
         nums[i] = False
         seq.append(i + 1)
+        end = False
 
-    while True:
+    # start the loop printing out all the sequences
+    while not end:
+        current_num = seq[index]
+
+        # print the until the last number reach the biggest possible number
         while True:
             print_list(seq)
             prv_num = current_num
             b_flag = True
             current_num += 1
+
+            # find the next number for the last sequence number
             while current_num <= n:
                 if nums[current_num - 1]:
                     nums[current_num - 1] = False
@@ -30,25 +38,39 @@ def print_sequences(m, n):
                     break
                 current_num += 1
             if b_flag:
-                # this is end of loop
+                # this will exit the first inner loop
                 break
             seq[index] = current_num
             nums[prv_num - 1] = True
 
-        # increment previous index number
-        loop_flag = True
-        while loop_flag:
+        # increment previous index numbers
+        backtrack = False
+        while not end and not backtrack:
+            nums[seq[index] - 1] = True
             index -= 1
+            if index < 0:
+                end = True
+                break
             prv_num = seq[index]
-            while seq[index] <= n:
+            while seq[index] < n:
                 seq[index] += 1
                 if nums[seq[index] - 1]:
-                    loop_flag = False
+                    backtrack = True
                     nums[seq[index] - 1] = False
                     nums[prv_num - 1] = True
                     break
-        # start the other later indices numbers to the smallest numbers
-        # code here
+
+            # set the rest of the numbers
+            if backtrack:
+                while index < m - 1:
+                    index += 1
+                    new_num = 0
+                    while new_num < n:
+                        new_num += 1
+                        if nums[new_num - 1]:
+                            seq[index] = new_num
+                            nums[new_num - 1] = False
+                            break
 
 
 if __name__ == '__main__':
