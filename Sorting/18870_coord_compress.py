8a5fb5e7fe9355db1819c3b4f83472533912620d
appleboy919 <doh24@wisc.edu>
2021-04-10 23:44:05 +0900

    new: coordinate compression

---
+++ Sorting/18870_coord_compress.py
@@ -0,0 +1,10 @@
+import sys
+
+
+def coord_compress(list):
+
+
+if __name__ == '__main__':
+    N = int(sys.stdin.readline())
+    Xs = list(map(int, sys.stdin.readline().split))
+    coord_compress(Xs)
