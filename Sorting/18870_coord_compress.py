6a154b483873e09bab3a1f4c9989f3b2b6df6c8d
appleboy919 <doh24@wisc.edu>
2021-04-11 17:19:10 +0900

    cont: sort the array with tuples of number and its indices

--- Sorting/18870_coord_compress.py
+++ Sorting/18870_coord_compress.py
@@ -2,9 +2,23 @@ import sys
 
 
 def coord_compress(list):
+    coord = []
+    index = 0
+    for i in list:
+        coord.append((i, index))
+        index += 1
+    coord.sort()
+    num = 0
+    current = coord[0][0]
+    for t in coord:
+        list[t[1]] = num
+
 
 
 if __name__ == '__main__':
     N = int(sys.stdin.readline())
-    Xs = list(map(int, sys.stdin.readline().split))
-    coord_compress(Xs)
+    Xs = list(map(int, sys.stdin.readline().split()))
+    Xs = coord_compress(Xs)
+    for i in Xs:
+        sys.stdout.write(str(i) + ' ')
+    sys.stdout.write('\n')
