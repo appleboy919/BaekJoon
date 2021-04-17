a644901430eedb32cb95ab86734601d845d37b4b
appleboy919 <doh24@wisc.edu>
2021-04-17 20:50:32 +0900

    cont: iterate while increasing the number of sequence

--- Backtracking/15650_N_seq_2.py
+++ Backtracking/15650_N_seq_2.py
@@ -2,6 +2,15 @@ import sys
 
 
 def n_sequences(n, m):
+    num_used = [True] * n
+    seq = []
+    for i in range(1, m + 1):
+        seq.append(i)
+        num_used[i - 1] = False
+    track_index = m - 1
+    while True:
+        current = seq[track_index]
+        while nums
 
 
 if __name__ == '__main__':
