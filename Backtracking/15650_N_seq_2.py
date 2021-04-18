ed1ef24c078d29fa61ac6617c57472c91777baa8
appleboy919 <doh24@wisc.edu>
2021-04-18 22:46:22 +0900

    cont: increment seq 2

--- Backtracking/15650_N_seq_2.py
+++ Backtracking/15650_N_seq_2.py
@@ -1,6 +1,12 @@
 import sys
 
 
+def print_seq(seq):
+    for i in seq:
+        sys.stdout.write(str(i) + ' ')
+    sys.stdout.write('\n')
+
+
 def n_sequences(n, m):
     num_used = [True] * n
     seq = []
@@ -8,9 +14,21 @@ def n_sequences(n, m):
         seq.append(i)
         num_used[i - 1] = False
     track_index = m - 1
+    i_flag = False
     while True:
         current = seq[track_index]
-        while nums
+        while current <= n:
+            # print sequence
+            print_seq(seq)
+
+            # increment seq number
+            current += 1
+            if num_used[current - 1]:
+                i_flag = True
+                seq[track_index] = current
+                break
+
+
 
 
 if __name__ == '__main__':
