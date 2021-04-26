115ed92f1cb6f869c4a90564533ada0bb0fab4b8
appleboy919 <doh24@wisc.edu>
2021-04-26 23:39:45 +0900

    cont: simplify the algorithm

--- Backtracking/15650_N_seq_2.py
+++ Backtracking/15650_N_seq_2.py
@@ -8,44 +8,16 @@ def print_seq(seq):
 
 
 def n_sequences(n, m):
-    num_used = [True] * n
-    seq = []
-    for i in range(1, m + 1):
-        seq.append(i)
-        num_used[i - 1] = False
-    track_index = m - 1
-    while True:
-        # print sequence
-        print_seq(seq)
+    seq = [i for i in range(1, m+1)]
 
-        # increment seq number
-        while True:
-            i_flag = False
-            current = seq[track_index]
-            while current <= n:
-                current += 1
-                if num_used[current - 1]:
-                    i_flag = True
-                    num_used[seq[track_index] - 1] = True
-                    num_used[current - 1] = False
-                    seq[track_index] = current
-                    break
-            if not i_flag:  # need back tracking
-                # back track 1
-                b_flag = False
-                while not b_flag:
-                    num_used[seq[track_index] - 1] = True
-                    track_index -= 1
-                    current = seq[track_index]
-                    while current <= n - (track_index - m + 1):
-                        current += 1
-                        if num_used[current - 1]:
-                            num_used[seq[track_index] - 1] = True
-                            seq[track_index] = current
-                            num_used[current - 1] = False
-                            b_flag = True
-                            break
-                if
+    while True:
+        track_index = m-1
+        current = seq[track_index]
+        while current <= n:
+            seq[track_index] = current
+            print_seq(seq)
+            current += 1
+
 
 
 
