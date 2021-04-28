89922eb64cf63b011cf07bb719001cc9c4084033
David Dongmin Oh <doh24@wisc.edu>
2021-04-28 11:15:13 +0900

    complete: simplified algorithm for printing combination

--- Backtracking/15650_N_seq_2.py
+++ Backtracking/15650_N_seq_2.py
@@ -9,18 +9,28 @@ def print_seq(seq):
 
 def n_sequences(n, m):
     seq = [i for i in range(1, m+1)]
-
-    while True:
+    rep_flag = True
+    while rep_flag:
         track_index = m-1
         current = seq[track_index]
         while current <= n:
             seq[track_index] = current
             print_seq(seq)
             current += 1
-
-
+        rep_flag = False
+        track_index -= 1
+        while track_index >= 0:
+            if seq[track_index] < n-(m-1-track_index):
+                seq[track_index] += 1
+                while track_index < m-1:
+                    track_index += 1
+                    seq[track_index] = seq[track_index-1] + 1
+                    rep_flag = True
+                break
+            track_index -= 1
+      
 
 
 if __name__ == '__main__':
-    N, M = sys.stdin.readline().split()
-    n_sequences(N, M)
+  N, M = map(int, sys.stdin.readline().split())
+  n_sequences(N, M)
