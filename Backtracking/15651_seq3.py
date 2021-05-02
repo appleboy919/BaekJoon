f37f406a6dfcd0a79e1422b5d0d5c53ee6d4e6cc
appleboy919 <doh24@wisc.edu>
2021-05-02 22:50:48 +0900

    complete: print combination sequence

--- Backtracking/15651_seq3.py
+++ Backtracking/15651_seq3.py
@@ -1,9 +1,32 @@
 import sys
 
+
+def print_list(l):
+    for i in l:
+        sys.stdout.write(str(i) + ' ')
+    sys.stdout.write('\n')
+
+
 def print_seq(m, n):
-  
+    seq = [1] * m
+    index = m - 1
+    loop_flag = True
+    while loop_flag:
+        while seq[index] <= n:
+            print_list(seq)
+            seq[index] += 1
+        loop_flag = False
+        while index > 0:
+            index -= 1
+            if seq[index] < n:
+                seq[index] += 1
+                while index < m - 1:
+                    index += 1
+                    seq[index] = 1
+                loop_flag = True
+                break
+
 
 if __name__ == '__main__':
-  N, M = map(int, sys.stdin.readline().split())
-  print_seq(M, N)
-  
+    N, M = map(int, sys.stdin.readline().split())
+    print_seq(M, N)
