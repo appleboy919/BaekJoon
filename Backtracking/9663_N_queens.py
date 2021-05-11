220d01023d30cdd91baadf3822f09ee3a71c3e69
appleboy919 <doh24@wisc.edu>
2021-05-11 16:47:30 +0900

    cont: need backtracking part for row-1

--- Backtracking/9663_N_queens.py
+++ Backtracking/9663_N_queens.py
@@ -1,10 +1,46 @@
 import sys
 
 
+def mark_col(board, row, col, n):
+    while row <= n - 1:
+        board[row][col] = False
+        row += 1
+
+
+def marK_diagonal(board, row, col, n):
+    while row < n and col < n:
+        board[row][col] = False
+        row += 1
+        col += 1
+
+
 def print_N_queens(n):
-    board = [True] * n * n
-    row = col = n - 1
+    board = [[True] * n] * n
+    ans = 0
+    init_col = 0
     # TODO: check diagonal??
+    row = 0
+
+    # start the outer loop
+    while True:
+        # initial position of q on (1,1)
+        board[row][init_col] = False
+
+        # mark diagonal, row to False
+        mark_col(board, row, init_col, n)
+        marK_diagonal(board, row, init_col, n)
+
+        # start the first inner loop for positioning queens on each row
+        while row < n:
+            row += 1
+            for col in range(n):
+                if board[row][col]:
+                    if row == n - 1:
+                        ans += 1
+                        continue
+                    mark_col(board, row, col, n)
+                    marK_diagonal(board, row, col, n)
+                    break
 
 
 if __name__ == '__main__':
