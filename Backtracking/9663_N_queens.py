16e9f498044f3762b901089b2b05d64f19f20ba0
appleboy919 <doh24@wisc.edu>
2021-05-20 21:19:06 +0900

    cont: right answers but need to reduce time

--- Backtracking/9663_N_queens.py
+++ Backtracking/9663_N_queens.py
@@ -1,51 +1,54 @@
 import sys
 
-
-def mark_pos(n, board, row, col, check):
-    # check cells on same column
-    for i in range(row + 1, n):
-        board[i][col] = check
-
-    # check diagonal cells
-    while row < n and col < n:
-        board[row][col] = check
-        row += 1
-        col += 1
+# check the current position
+def check_pos(n, cols, row, col):
+    col_num = 0
+    while row > 0:
+        row -= 1
+        col_num += 1
+        if cols[row] == col:
+            return False
+        if col - col_num >= 0 and cols[row] == col - col_num:
+            return False
+        if col - col_num < n and cols[row] == col + col_num:
+            return False
+    return True
 
 
 def N_queens(n):
-    colPos = [0] * n
-    board = [[True] * n] * n
-    row = col = 0
+    colPos = [-1] * n
+    row = 0
     ans = 0
+
+    # start the outer loop
     while True:
+
+        # check each cell in the current row
         for i in range(n):
-            if board[row][i]:
+            if check_pos(n, colPos, row, i):
+                # n-th queen has been placed --> +1
                 if row == n - 1:
-                    # test
-                    print('+1 at', str(row), str(col))
                     ans += 1
                     continue
                 colPos[row] = i
-                mark_pos(n, board, row, i, False)
                 break
 
         # backtrack starts here
-        if row == n - 1:
+        if colPos[row] == -1:
             end_bt = False
-            while row >= 0 and not end_bt:
+            while row > 0 and not end_bt:
                 row -= 1
-                for i in range(colPos[row], n):
-                    if board[row][i]:
+                for i in range(colPos[row] + 1, n):
+                    if check_pos(n, colPos, row, i):
                         end_bt = True
                         colPos[row] = i
-                        mark_pos(n, board, row, col, False)
                         break
                 if not end_bt:
-                    mark_pos(n, board, row, colPos[row], True)
+                    colPos[row] = -1
             if not end_bt and row == 0:
                 break
         row += 1
+    return ans
 
 
 if __name__ == '__main__':
