0f738c52dfca352ffd33b6c8790a713a47fecdd2
appleboy919 <doh24@wisc.edu>
2021-06-10 17:33:57 +0900

    cont: problem was on getline

--- Backtracking/2580_sudoku.cpp
+++ Backtracking/2580_sudoku.cpp
@@ -1,10 +1,26 @@
 #include <iostream>
 #include <list>
 #include <string>
-#define MAX_BLANK 64
+int static MAX_BLANK = 81;
 // #include <time.h>
 
 using namespace std;
+
+bool check(int sudoku[9][9], int row, int col, int num) {
+    int rowNum, colNum;
+    for (int i = 0; i < 9; i++)
+        if (sudoku[row][i] == num || sudoku[i][col] == num)
+            return false;
+
+    int row_i = row / 3 * 3;
+    int col_i = col / 3 * 3;
+    for (int i = row_i; i < row_i + 3; i++) {
+        for (int j = col_i; j < col_i + 3; j++)
+            if (sudoku[i][j] == num)
+                return false;
+    }
+    return true;
+}
 
 void find_number(int sudoku[9][9], int row, int col, bool nums[9]) {
     int rowNum, colNum;
@@ -25,24 +41,37 @@ void find_number(int sudoku[9][9], int row, int col, bool nums[9]) {
                 nums[sudoku[i][j] - 1] = false;
     }
 }
-bool fill_sudoku(int sudoku[9][9], int blank[MAX_BLANK][2], int num,
-                 int index) {
+bool fill_sudoku(int sudoku[9][9], int blank[MAX_BLANK][2], int num, int index,
+                 bool nums[9]) {
     if (index == num)
         return true;
-    bool nums[9] = {0};
-    for (int i = 0; i < 9; i++)
-        nums[i] = true;
+    if (index != 0 && blank[index - 1][0] != blank[index][0]) {
+        for (int i = 0; i < 9; i++)
+            nums[i] = true;
+        find_number(sudoku, blank[index][0], blank[index][1], nums);
+    }
+
+    // bool nums[9] = {0};
     // string temp = *itr;
-    int pos1 = blank[index][0];
-    int pos2 = blank[index][1];
-    find_number(sudoku, pos1, pos2, nums);
+    // find_number(sudoku, pos1, pos2, nums);
     // int temp_sudoku[9][9];
     // copy(&sudoku[0][0], &sudoku[0][0] + 9 * 9, &temp_sudoku[0][0]);
+    // for (int i = 1; i < 10; i++) {
+    //     if (check(sudoku, row, col, i)) {
+    //         sudoku[row][col] = i;
+    //         if (fill_sudoku(sudoku, blank, num, index + 1))
+    //             return true;
+    //     }
+    // }
     for (int i = 0; i < 9; i++) {
         if (nums[i]) {
-            sudoku[pos1][pos2] = i + 1;
-            if (fill_sudoku(sudoku, blank, num, index + 1))
+            sudoku[blank[index][0]][blank[index][1]] = i + 1;
+            if (fill_sudoku(sudoku, blank, num, index + 1, nums)) {
+                cout << "reached the end with " << index << "("
+                     << blank[index][0] << ", " << blank[index][1] << ") --> "
+                     << i + 1 << endl;
                 return true;
+            }
         }
     }
     return false;
@@ -54,11 +83,14 @@ int main(void) {
     int num = 0;
     int blank[MAX_BLANK][2];
     int index;
+    bool nums[9] = {1};
     string t;
+
     for (int i = 0; i < 9; i++) {
         index = 0;
         getline(cin, t);
-        for (int j = 0; j < 17; j++) {
+        for (int j = 0; j < 18; j++) {
+            cout << "\n in\t" << t[j];
             if (t[j] != ' ') {
                 if (t[j] == '0') {
                     // blank.push_back(to_string(i) + to_string(index));
@@ -70,9 +102,13 @@ int main(void) {
                 index++;
             }
         }
+        cout << endl;
+        cout << "!!" << num << endl;
     }
-    fill_sudoku(sudoku, blank, num, 0);
-    // cout << endl;
+    cout << endl << num << endl;
+
+    fill_sudoku(sudoku, blank, num, 0, nums);
+    cout << endl;
     for (int i = 0; i < 9; i++) {
         for (int j = 0; j < 9; j++)
             cout << sudoku[i][j] << " ";
