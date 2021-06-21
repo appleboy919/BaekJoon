976f7ee503275b01f77819b959f4f6a4200cac3e
appleboy919 <doh24@wisc.edu>
2021-06-21 22:22:43 +0900

    new: MIN, MAX of operator combinations

---
+++ Backtracking/14888_operator.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#define OPR_NUM 4
+using namespace std;
+int main() {
+    int n;
+    cin >> n;
+    int nums[n], operators[OPR_NUM];
+    for (int i = 0; i < n; i++)
+        cin >> nums[i];
+    for (int i = 0; i < OPR_NUM; i++)
+        cin >> operators[i];
+    /*
+    for (int i = 0; i < n; i++)
+        cout << nums[i] << " ";
+    cout << endl;
+    for (int i = 0; i < 4; i++)
+        cout << operators[i] << " ";
+    cout << endl;
+    */
+    return 0;
+}
