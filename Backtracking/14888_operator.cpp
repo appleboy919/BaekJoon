9c7d93d5bb5640f4f98bc1a8147cc027b1a0a55d
appleboy919 <doh24@wisc.edu>
2021-06-23 23:39:08 +0900

    cont: define function 2

--- Backtracking/14888_operator.cpp
+++ Backtracking/14888_operator.cpp
@@ -2,15 +2,27 @@
 #define OPR_NUM 4
 using namespace std;
 
-int *minMaxNum(int nums) {}
+int *minMaxNum(int *nums, int num, int opr[OPR_NUM], int *temp) { return }
 int main() {
     int n;
     cin >> n;
-    int nums[n], operators[OPR_NUM];
+    int nums[n], operators[OPR_NUM], temp[n - 1];
     for (int i = 0; i < n; i++)
         cin >> nums[i];
     for (int i = 0; i < OPR_NUM; i++)
         cin >> operators[i];
+    int idx = 0;
+    for (int i = 0; i < OPR_NUM; i++) {
+        for (int j = 0; j < operators[i]; j++) {
+            temp[idx] = i;
+            idx++;
+        }
+    }
+    /*
+        for (int i = 0; i < n - 1; i++)
+            cout << temp[i] << " ";
+        cout << endl;
+        */
     /*
     for (int i = 0; i < n; i++)
         cout << nums[i] << " ";
