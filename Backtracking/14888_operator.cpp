9dfade81c0a55482f19588087101d307f6b96e0e
appleboy919 <doh24@wisc.edu>
2021-06-25 22:03:49 +0900

    cont: function definition

--- Backtracking/14888_operator.cpp
+++ Backtracking/14888_operator.cpp
@@ -12,29 +12,53 @@ int calculate(int *nums, int *temp_opr, int num) {
         case 2:
             ans *= nums[i];
         case 3:
+            ans /= nums[i];
         }
     }
+    return ans;
 }
-int *minMaxNum(int *nums, int num, int opr[4], int *temp_opr, int *opr_rmd,
-               int ans[2]) {
-    if (nums)
-        return nums return
+void maxMinNum(int *nums, int num, int opr[4], int *temp_opr, int ans[2]) {
+    // 1. calculate the numbers from the passed sequence
+    int res = calculate(nums, temp_opr, num);
+
+    // 2. get the next sequence of operators
+
+    int idx = num - 2;
+    int temp[4] = {0};
+    while (idx >= 0) {
+        temp[temp_opr[idx]]++;
+        idx--;
+        temp_opr[idx];
+    }
+    if (ans[0] < res)
+        ans[0] = res;
+    if (ans[1] > res)
+        ans[1] = res;
+    return;
 }
 int main() {
     int n;
     cin >> n;
-    int nums[n], operators[4], temp[n - 1];
+    int nums[n], operators[4], temp[n - 1], ans[2];
     for (int i = 0; i < n; i++)
         cin >> nums[i];
     for (int i = 0; i < 4; i++)
         cin >> operators[i];
     int idx = 0;
-    for (int i = 0; i < OPR_NUM; i++) {
+    for (int i = 0; i < 4; i++) {
         for (int j = 0; j < operators[i]; j++) {
             temp[idx] = i;
             idx++;
         }
     }
+    int remainder[4] = {0};
+    idx = n - 2;
+    while (idx >= 0) {
+        remainder[temp[idx]]++;
+        }
+    remainder[operators[n - 2]]++;
+    maxMinNum(nums, n, remainder, temp, ans);
+
     /*
         for (int i = 0; i < n - 1; i++)
             cout << temp[i] << " ";
