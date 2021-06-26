720cd0acd26fc8c4e8aa912ff6ebdb8704e5d2e5
appleboy919 <doh24@wisc.edu>
2021-06-26 15:52:01 +0900

    solved: backtrack with operator as numbers

--- Backtracking/14888_operator.cpp
+++ Backtracking/14888_operator.cpp
@@ -1,76 +1,55 @@
 #include <iostream>
+
 using namespace std;
 
+int maxRes = -1000000000;
+int minRes = 1000000000;
 int calculate(int *nums, int *temp_opr, int num) {
     int ans = nums[0];
     for (int i = 1; i < num; i++) {
         switch (temp_opr[i - 1]) {
         case 0:
             ans += nums[i];
+            break;
         case 1:
             ans -= nums[i];
+            break;
         case 2:
             ans *= nums[i];
+            break;
         case 3:
             ans /= nums[i];
         }
     }
     return ans;
 }
-void maxMinNum(int *nums, int num, int opr[4], int *temp_opr, int ans[2]) {
-    // 1. calculate the numbers from the passed sequence
-    int res = calculate(nums, temp_opr, num);
-
-    // 2. get the next sequence of operators
-
-    int idx = num - 2;
-    int temp[4] = {0};
-    while (idx >= 0) {
-        temp[temp_opr[idx]]++;
-        idx--;
-        temp_opr[idx];
+void maxMinNum(int *nums, int num, int opr[4], int *temp_opr, int index) {
+    if (index == num - 1) {
+        int ans = calculate(nums, temp_opr, num);
+        if (maxRes < ans)
+            maxRes = ans;
+        if (minRes > ans)
+            minRes = ans;
+        return;
     }
-    if (ans[0] < res)
-        ans[0] = res;
-    if (ans[1] > res)
-        ans[1] = res;
-    return;
+    for (int i = 0; i < 4; i++) {
+        if (opr[i] != 0) {
+            opr[i]--;
+            temp_opr[index] = i;
+            maxMinNum(nums, num, opr, temp_opr, index + 1);
+            opr[i]++;
+        }
+    }
 }
 int main() {
     int n;
     cin >> n;
-    int nums[n], operators[4], temp[n - 1], ans[2];
+    int nums[n], operators[4], temp[n - 1];
     for (int i = 0; i < n; i++)
         cin >> nums[i];
     for (int i = 0; i < 4; i++)
         cin >> operators[i];
-    int idx = 0;
-    for (int i = 0; i < 4; i++) {
-        for (int j = 0; j < operators[i]; j++) {
-            temp[idx] = i;
-            idx++;
-        }
-    }
-    int remainder[4] = {0};
-    idx = n - 2;
-    while (idx >= 0) {
-        remainder[temp[idx]]++;
-        }
-    remainder[operators[n - 2]]++;
-    maxMinNum(nums, n, remainder, temp, ans);
-
-    /*
-        for (int i = 0; i < n - 1; i++)
-            cout << temp[i] << " ";
-        cout << endl;
-        */
-    /*
-    for (int i = 0; i < n; i++)
-        cout << nums[i] << " ";
-    cout << endl;
-    for (int i = 0; i < 4; i++)
-        cout << operators[i] << " ";
-    cout << endl;
-    */
+    maxMinNum(nums, n, operators, temp, 0);
+    cout << maxRes << endl << minRes << endl;
     return 0;
 }
