8f167786bdd96a06c4925305a5e1dcebdd97e542
appleboy919 <doh24@wisc.edu>
2021-06-28 23:49:12 +0900

    cont: 2d array passing

--- Backtracking/14889_team.cpp
+++ Backtracking/14889_team.cpp
@@ -2,21 +2,47 @@
 using namespace std;
 int minVal = 10000;
 
-void minDiff(int n, int *s, int idx) {}
+void minDiff(int n, int **s, int idx, int *temp, int t1, int total) {
+    if (idx == n / 2) {
+        cout << "t1: " << t1 << "!\n";
+        int diff = abs(total - t1 * 2);
+        if (minVal > diff)
+            minVal = diff;
+        return;
+    }
+    for (int i = idx; i < n - idx; i++) {
+        int new_t1 = t1;
+        temp[idx] = i;
+        for (int j = 0; j < idx; j++) {
+            new_t1 += s[temp[j]][i];
+            new_t1 += s[i][temp[j]];
+        }
+        minDiff(n, s, idx + 1, temp, new_t1, total);
+    }
+}
 int main() {
     int n;
+    int sum = 0;
     cin >> n;
-    int s[n][n];
-    for (int i = 0; i < n; i++)
-        for (int j = 0; j < n; j++)
-            cin >> s[i][j];
-
+    int **s;
+    s = new int *[n];
     for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++)
-            cout << s[i][j] << " ";
-        cout << endl;
+        s[i] = new int[n];
+        for (int j = 0; j < n; j++) {
+            cin >> s[i][j];
+            sum += s[i][j];
+        }
     }
-    cout << endl;
+    cout << "sum: " << sum << endl;
+    // for (int i = 0; i < n; i++) {
+    //     for (int j = 0; j < n; j++)
+    //         cout << s[i][j] << " ";
+    //     cout << endl;
+    // }
+    int temp[n / 2];
+    temp[0] = 0;
 
+    minDiff(n, s, 1, temp, 0, sum);
+    cout << minVal << endl;
     return 0;
 }
