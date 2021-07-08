42a3bdf7db192c8c691c4bce1b2fb7e9e8aed199
appleboy919 <doh24@wisc.edu>
2021-07-08 21:59:46 +0900

    cont: need to reduce time

--- DP/1003_fibonacci.cpp
+++ DP/1003_fibonacci.cpp
@@ -1,15 +1,26 @@
 #include <iostream>
 using namespace std;
 
-int fibonacci(int n) {
-    // how to code DP?
-    return 0;
+void fib(int *x, int n) {
+    if (n == 1 || n == 0) {
+        x[n]++;
+        return;
+    }
+    fib(x, n - 1);
+    fib(x, n - 2);
 }
 
 int main() {
 
-    int n;
-    cin >> n;
-    fibonacci(n);
+    int t;
+    cin >> t;
+    int n[t];
+    for (int i = 0; i < t; i++)
+        cin >> n[i];
+    for (int i = 0; i < t; i++) {
+        int oneTwo[2] = {0};
+        fib(oneTwo, n[i]);
+        cout << oneTwo[0] << " " << oneTwo[1] << endl;
+    }
     return 0;
 }
