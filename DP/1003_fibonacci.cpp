91d55f8bbf6d81fbd98d8663ff2e689cdbc4cf38
appleboy919 <doh24@wisc.edu>
2021-07-09 23:23:55 +0900

    solved: use memoization to reduce running time

--- DP/1003_fibonacci.cpp
+++ DP/1003_fibonacci.cpp
@@ -1,26 +1,34 @@
 #include <iostream>
 using namespace std;
+int nums[41][2] = {0};
 
-void fib(int *x, int n) {
-    if (n == 1 || n == 0) {
-        x[n]++;
-        return;
+void fib(int n) {
+    int idx;
+    for (int i = n; i >= 0; i--) {
+        if (nums[i][0] != 0 || nums[i][1] != 0) {
+            idx = i;
+            break;
+        }
     }
-    fib(x, n - 1);
-    fib(x, n - 2);
+    for (int i = idx + 1; i <= n; i++) {
+        nums[i][0] = nums[i - 1][0] + nums[i - 2][0];
+        nums[i][1] = nums[i - 1][1] + nums[i - 2][1];
+    }
 }
 
 int main() {
-
+    nums[0][0] = 1;
+    nums[1][1] = 1;
     int t;
     cin >> t;
     int n[t];
     for (int i = 0; i < t; i++)
         cin >> n[i];
     for (int i = 0; i < t; i++) {
-        int oneTwo[2] = {0};
-        fib(oneTwo, n[i]);
-        cout << oneTwo[0] << " " << oneTwo[1] << endl;
+        if (n[i] != 0 && n[i] != 1 && nums[n[i]][0] == 0)
+            fib(n[i]);
+        cout << nums[n[i]][0] << " " << nums[n[i]][1] << endl;
     }
+
     return 0;
 }
