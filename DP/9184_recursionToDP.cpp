8bac363aaf0421076e6690b7dfabae836db32238
appleboy919 <doh24@wisc.edu>
2021-07-11 23:39:43 +0900

    solved: solved with turbulation

--- DP/9184_recursionToDP.cpp
+++ DP/9184_recursionToDP.cpp
@@ -7,11 +7,16 @@ int w(int x1, int x2, int x3) {
     if (x1 <= 0 || x2 <= 0 || x3 <= 0)
         return 1;
     if (x1 > 20 || x2 > 20 || x3 > 20)
-        return n[20][20][20];
+        return w(20, 20, 20);
+    if (n[x1][x2][x3] != 0)
+        return n[x1][x2][x3];
     if (x1 < x2 && x2 < x3)
-        return w(x1, x2, x3 - 1) + w(x1, x2 - 1, x3 - 1) - w(x1, x2 - 1, x3);
-    return w(x1 - 1, x2, x3) + w(x1 - 1, x2 - 1, x3) + w(x1 - 1, x2, x3 - 1) -
-           w(x1 - 1, x2 - 1, x3 - 1);
+        n[x1][x2][x3] =
+            w(x1, x2, x3 - 1) + w(x1, x2 - 1, x3 - 1) - w(x1, x2 - 1, x3);
+    else
+        n[x1][x2][x3] = w(x1 - 1, x2, x3) + w(x1 - 1, x2 - 1, x3) +
+                        w(x1 - 1, x2, x3 - 1) - w(x1 - 1, x2 - 1, x3 - 1);
+    return n[x1][x2][x3];
 }
 
 int main() {
@@ -21,10 +26,10 @@ int main() {
     while (true) {
         temp = new int[3];
         cin >> temp[0] >> temp[1] >> temp[2];
-        nums.push_back(temp);
-        num++;
         if (temp[0] == -1 && temp[1] == -1 && temp[2] == -1)
             break;
+        nums.push_back(temp);
+        num++;
     }
     int ans;
     for (int i = 0; i < num; i++) {
