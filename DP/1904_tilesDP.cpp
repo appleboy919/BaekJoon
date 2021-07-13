207cc5c4135c535f65e5ddbf511a5f71b395a2e5
appleboy919 <doh24@wisc.edu>
2021-07-13 22:56:02 +0900

    solved: problem was on % 15746

--- DP/1904_tilesDP.cpp
+++ DP/1904_tilesDP.cpp
@@ -1,11 +1,25 @@
 #include <iostream>
 using namespace std;
 
-int tile(int n, int dp[]) { return 0; }
+int tile(int n, int dp[]) {
+    if (n / 2 == 0)
+        return 1;
+    // use '1'
+    if (dp[n - 1] == 0)
+        dp[n - 1] = tile(n - 1, dp);
+    // use '00'
+    if (dp[n - 2] == 0)
+        dp[n - 2] = tile(n - 2, dp);
+    dp[n] = (dp[n - 1] + dp[n - 2])%15746;
+    return dp[n];
+}
 
 int main() {
     int n;
     cin >> n;
-
+    int *dp = new int[n + 1];
+    int ans = tile(n, dp);
+    cout << ans << endl;
+    delete[] dp;
     return 0;
 }
