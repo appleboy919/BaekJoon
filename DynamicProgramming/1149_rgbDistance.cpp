06321cf0a12770519f77a64d3364e3f080362076
appleboy919 <doh24@wisc.edu>
2021-07-18 23:10:47 +0900

    cont: basic settings

--- DynamicProgramming/1149_rgbDistance.cpp
+++ DynamicProgramming/1149_rgbDistance.cpp
@@ -2,6 +2,13 @@
 using namespace std;
 
 int main() {
+    int n;
+    cin >> n;
+    int val[n][3];
+    for (int i = 0; i < n; i++)
+        cin >> val[i][0] >> val[i][1] >> val[i][2];
+    for (int i = 0; i < n; i++)
+        cout << val[i][0] << " " << val[i][1] << " " << val[i][2] << endl;
 
     return 0;
 }
