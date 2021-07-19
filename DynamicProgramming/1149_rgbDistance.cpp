3ab09fd3c37c064eb0dda1e167516b5795413046
David Dongmin Oh <doh24@wisc.edu>
2021-07-19 23:24:06 +0900

    cont: how to proceed DP

--- DynamicProgramming/1149_rgbDistance.cpp
+++ DynamicProgramming/1149_rgbDistance.cpp
@@ -1,14 +1,19 @@
 #include <iostream>
 using namespace std;
+int minVal;
+void paint(int n, int **c, int idx) {
+
+}
 
 int main() {
     int n;
     cin >> n;
-    int val[n][3];
+    minVal = 1000*n;
+    int c[n][3];
     for (int i = 0; i < n; i++)
-        cin >> val[i][0] >> val[i][1] >> val[i][2];
+        cin >> c[i][0] >> c[i][1] >> c[i][2];
     for (int i = 0; i < n; i++)
-        cout << val[i][0] << " " << val[i][1] << " " << val[i][2] << endl;
+        cout << c[i][0] << " " << c[i][1] << " " << c[i][2] << endl;
 
     return 0;
 }
