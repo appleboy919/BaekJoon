0260fd6c88f8b10778ce75e9d941e06acafc90ba
appleboy919 <doh24@wisc.edu>
2021-07-20 10:42:03 +0900

    solved: save each best decision in 2d array

--- DynamicProgramming/1149_rgbDistance.cpp
+++ DynamicProgramming/1149_rgbDistance.cpp
@@ -1,19 +1,47 @@
 #include <iostream>
 using namespace std;
-int minVal;
-void paint(int n, int **c, int idx) {
-
+int **c;
+int **t;
+int paint(int n, int color, int idx) {
+    if (idx == n)
+        return 0;
+    if (t[idx][color] == 0) {
+        int temp;
+        int ret = -1;
+        for (int i = 0; i < 3; i++) {
+            if (idx != 0 && i == color)
+                continue;
+            temp = paint(n, i, idx + 1) + c[idx][i];
+            if (ret == -1)
+                ret = temp;
+            else
+                ret = ret > temp ? temp : ret;
+        }
+        t[idx][color] = ret;
+    }
+    return t[idx][color];
 }
 
 int main() {
     int n;
     cin >> n;
-    minVal = 1000*n;
-    int c[n][3];
+    int ans;
+    c = new int *[n];
+    t = new int *[n];
+    for (int i = 0; i < n; i++) {
+        c[i] = new int[3];
+        t[i] = new int[3];
+    }
     for (int i = 0; i < n; i++)
         cin >> c[i][0] >> c[i][1] >> c[i][2];
-    for (int i = 0; i < n; i++)
-        cout << c[i][0] << " " << c[i][1] << " " << c[i][2] << endl;
-
+    for (int i = 0; i < 3; i++)
+        ans = paint(n, 0, 0);
+    cout << ans << endl;
+    for (int i = 0; i < n; i++) {
+        delete[] c[i];
+        delete[] t[i];
+    }
+    delete[] c;
+    delete[] t;
     return 0;
 }
