f39762643c934af630b97005031dd74ee05edb56
appleboy919 <doh24@wisc.edu>
2021-07-23 01:01:04 +0900

    solved: divide each specification with memoization

--- DynamicProgramming/2579_stairs.cpp
+++ DynamicProgramming/2579_stairs.cpp
@@ -1,12 +1,40 @@
 #include <iostream>
 using namespace std;
-int goUp(int n, int *c, int f) { return 0; }
+long int **dp;
+long int goUp(int n, int *c, int f, int prvStep) {
+    if (f > n)
+        return -1000 * n;
+    if (f == n)
+        return c[n - 1];
+    if (dp[f - 1][prvStep - 1] == 0) {
+        long int t1 = -1;
+        long int t2;
+        if (prvStep != 1 || f == 1)
+            t1 = goUp(n, c, f + 1, 1) + c[f - 1];
+        t2 = goUp(n, c, f + 2, 2) + c[f - 1];
+        dp[f - 1][prvStep - 1] = t1 > t2 ? t1 : t2;
+    }
+    return dp[f - 1][prvStep - 1];
+}
 int main() {
     int n;
     cin >> n;
-    int c[n] = {0};
-    for (int i = 0; i < n; i++)
+    int *c = new int[n];
+    dp = new long int *[n];
+    for (int i = 0; i < n; i++) {
+        dp[i] = new long int[2];
         cin >> c[i];
+    }
+    long int t1 = goUp(n, c, 1, 1);
+    long int t2 = 0;
+    if (n >= 2)
+        t2 = goUp(n, c, 2, 2);
+    long int ans = t1 > t2 ? t1 : t2;
+    cout << ans << endl;
 
+    for (int i = 0; i < n; i++)
+        delete[] dp[i];
+    delete[] dp;
+    delete[] c;
     return 0;
 }
