25f4a1b4f90321c78ecae98d7580d61c0419b8ef
appleboy919 <doh24@wisc.edu>
2021-07-22 23:40:43 +0900

    new: go up stairs with max cost

---
+++ DynamicProgramming/2579_stairs.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int goUp(int n, int *c, int f) { return 0; }
+int main() {
+    int n;
+    cin >> n;
+    int c[n] = {0};
+    for (int i = 0; i < n; i++)
+        cin >> c[i];
+
+    return 0;
+}
