40473d6124b9079ceda95f12ad06b22fab91b078
appleboy919 <doh24@wisc.edu>
2021-07-29 15:32:00 +0900

    cont: change algorithM

--- DynamicProgramming/2156_wine.cpp
+++ DynamicProgramming/2156_wine.cpp
@@ -2,6 +2,21 @@
 using namespace std;
 int dp[10001] = {0};
 int max(int n1, int n2) { return n1 > n2 ? n1 : n2; }
+void sol(int n, int *c) {
+    dp[0] = c[0];
+    if (n == 1)
+        return;
+    dp[1] = c[0] + c[1];
+    if (n == 2)
+        return;
+    dp[2] = max(c[0] + c[2], c[1] + c[2]);
+    for (int i = 3; i < n; i++) {
+        dp[i] = max(dp[i - 3] + c[i - 1] + c[i], dp[i - 2] + c[i]);
+        // cout << dp[i] << " ";
+    }
+    // cout << endl;
+}
+
 // int solve(int n, int *c, int idx, int cont) {
 //     if (idx > n)
 //         return 0;
@@ -15,20 +30,6 @@ int max(int n1, int n2) { return n1 > n2 ? n1 : n2; }
 //     cout << dp[idx] << " ";
 //     return dp[idx];
 // }
-void sol(int n, int *c) {
-    dp[0] = c[0];
-    if (n == 1)
-        return;
-    dp[1] = c[0] + c[1];
-    if (n == 2)
-        return;
-    dp[2] = max(c[0] + c[2], c[1] + c[2]);
-    for (int i = 3; i < n; i++) {
-        dp[i] = max(dp[i - 3] + c[i - 1] + c[i], dp[i - 2] + c[i]);
-        cout << dp[i] << " ";
-    }
-    cout << endl;
-}
 int main() {
     int n;
     cin >> n;
@@ -37,6 +38,9 @@ int main() {
         cin >> c[i];
     // solve(n, c, 0, 0);
     sol(n, c);
-    cout << dp[n - 1] << endl;
+    int ans = dp[n - 1];
+    if (n > 2)
+        ans = dp[n - 2] > dp[n - 1] ? dp[n - 2] : dp[n - 1];
+    cout << ans << endl;
     return 0;
 }
