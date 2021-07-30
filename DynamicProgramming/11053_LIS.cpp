0be69b3aba0e9234498aa6e55589cf2e3dead702
appleboy919 <doh24@wisc.edu>
2021-07-31 00:24:05 +0900

    cont: figure out subproblems

--- DynamicProgramming/11053_LIS.cpp
+++ DynamicProgramming/11053_LIS.cpp
@@ -1,14 +1,31 @@
 #include <iostream>
 using namespace std;
-int dp[1001];
-void solve() {}
+int max_len = -1;
+int dp[1001] = {0};
+int solve(int n, int *c, int idx) {
+    if (idx == n)
+        return 1;
+    if (dp[idx] == 0) {
+        int temp = solve(n, c, idx + 1);
+        if (c[idx] < c[idx + 1] && c[idx] < c[0]) {
+            temp += 1;
+            c[0] = c[idx];
+        }
+        dp[idx] = temp;
+    }
+    return dp[idx];
+}
 
 int main() {
     int n;
     cin >> n;
-    int c[n];
-    for (int i = 0; i < n; i++)
+    int c[n + 1];
+    c[0] = 1001;
+    for (int i = 1; i < n + 1; i++)
         cin >> c[i];
-
+    for (int i = 0; i < n + 1; i++)
+        cout << c[i] << " ";
+    cout << endl;
+    cout << solve(n, c, 1) << endl;
     return 0;
 }
