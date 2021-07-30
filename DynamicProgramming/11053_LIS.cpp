a8095c3dc74a6df46f7a31e4c41ca27f1bc19acf
appleboy919 <doh24@wisc.edu>
2021-07-30 16:35:43 +0900

    new: get LIS(longest increasing subsequence) size

---
+++ DynamicProgramming/11053_LIS.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int dp[1001];
+void solve() {}
+
+int main() {
+    int n;
+    cin >> n;
+    int c[n];
+    for (int i = 0; i < n; i++)
+        cin >> c[i];
+
+    return 0;
+}
