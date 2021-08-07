2272ab04b0ba4a24d78a5391d0c8af18021bc7d5
appleboy919 <doh24@wisc.edu>
2021-08-07 23:52:32 +0900

    cont

--- DynamicProgramming/11053_LIS.cpp
+++ DynamicProgramming/11053_LIS.cpp
@@ -1,20 +1,17 @@
 #include <iostream>
 using namespace std;
-int dp[1001] = {0};
+int dp[1001][2];
 int c[1001];
 int n, ans;
-int solve(int n, int *c, int idx) {
-    if (idx == n)
-        return 1;
-    if (dp[idx] == 0) {
-        int temp = solve(n, c, idx + 1);
-        if (c[idx] < c[idx + 1] && c[idx] < c[0]) {
-            temp += 1;
-            c[0] = c[idx];
-        }
-        dp[idx] = temp;
+void solve(int idx) {
+    if (idx == n) {
+        dp[n][0] = 1;
+        dp[n][1] = c[idx];
+        return dp[n][0];
     }
-    return dp[idx];
+    solve(idx + 1);
+    if (c[idx] < dp[idx + 1][1]) {
+    }
 }
 
 int tempLIS(int idx) {
@@ -28,13 +25,14 @@ int tempLIS(int idx) {
     }
     return size;
 }
-int nextLIS(int idx) {
-    for (int i = idx + 1; i <= n; i++) {
-        if (c[idx] < c[i])
-            return dp[c[i]];
-    }
-    return 0;
-}
+// int nextLIS(int idx) {
+//     for (int i = idx + 1; i <= n; i++) {
+//         if (c[idx] < c[i])
+//             return dp[c[i]];
+//     }
+//     return 0;
+// }
+/*
 void lis(int idx) {
     if (idx == n) {
         cout << idx << ": 1!" << endl;
@@ -57,7 +55,9 @@ void lis(int idx) {
     cout << dp[c[idx]] << " / " << t << endl;
     // return dp[c[idx]] > t ? dp[c[idx]] : t;
 }
+*/
 int main() {
+    fill_n(&dp[0][0], 1001 * 2, 0);
     cin >> n;
     for (int i = 1; i <= n; i++)
         cin >> c[i];
