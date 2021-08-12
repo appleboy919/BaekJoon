56cad21539485e99244bd064e1d6551f00435f8f
appleboy919 <doh24@wisc.edu>
2021-08-12 12:46:55 +0900

    cont: how to handle upDown switch

--- DynamicProgramming/11054_bitonic.cpp
+++ DynamicProgramming/11054_bitonic.cpp
@@ -4,19 +4,35 @@ int dp[1001] = {0};
 int c[1001];
 int n, ans;
 void solve() {
-    dp[1] = 1;
-    int t;
+    int t = 0;
+    bool up;
     for (int i = 2; i <= n; i++) {
-        t = 0;
-        for (int j = 1; j < i; j++) {
-            // 2 for loop?
+        // how to handle bool??
+        if (c[i] == c[i - 1])
+            continue;
+        if (up) {
+            if (c[i - 1] > c[i])
+                up = false;
+            dp[i] = dp[i - 1] + 1;
+        } else {
+            if (c[i - 1] < c[i]) {
+                up = true;
+                dp[i] = 2;
+            } else
+                dp[i] = dp[i - 1] + 1;
         }
+        t = dp[i] > t ? dp[i] : t;
     }
+    ans = t;
 }
 int main() {
-    ans = 0;
+    fill_n(dp, 1001, 1);
     cin >> n;
     for (int i = 1; i <= n; i++)
         cin >> c[i];
+    for (int i = 1; i <= n; i++)
+        cout << dp[i] << endl;
+    cout << endl;
+    cout << ans << endl;
     return 0;
 }
